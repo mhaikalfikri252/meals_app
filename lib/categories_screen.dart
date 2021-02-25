fb3990206c428fff6ba71425dad8e18bf3663334
mhaikalfikri252 <mhaikalfikri252@gmail.com>
2021-02-25 17:54:27 +0700

    first commit

---
+++ lib/categories_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: [],
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(),
+    );
+  }
+}
